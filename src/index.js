063f9222b0b4aacb134afc3a2a1097723a45e41b
Pedro <pedromoratelli@gmail.com>
2020-03-30 00:38:05 -0300

    Install React Navigation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 
 import './config/ReactotronConfig';
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Hello World!</Text>
-    </View>
-  );
+  return <Routes />;
 }
