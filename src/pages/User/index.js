73801125f0f176d0e05b01be81bde2200ecb6799
Pedro <pedromoratelli@gmail.com>
2020-03-31 02:43:23 -0300

    Create Navigation to User, Add PropTypes

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 
-export default function User() {
+export default function User({ navigation }) {
+  console.tron.log(navigation.getParam('user'));
   return <View />;
 }
