616739f28724dfba2872361861a5ebac263ba747
Pedro <pedromoratelli@gmail.com>
2020-03-31 03:03:46 -0300

    Style Header in User Component

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
 import api from '../../services/api';
+
+import { Container, Header, Avatar, Name, Bio } from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -28,7 +29,19 @@ export default class User extends Component {
   }
 
   render() {
+    const { navigation } = this.props;
     const { stars } = this.state;
-    return <View />;
+
+    const user = navigation.getParam('user');
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+      </Container>
+    );
   }
 }
